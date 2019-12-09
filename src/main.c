be77ca2868bb2f35efd8f2794f9fb2a43c2a22aa
Leo Nunner <leo@leonunner.com>
2019-12-09 09:25:18 +0100

    Add LICENSE

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ int main() {
     Chip8 *state = calloc(1, sizeof(Chip8));
     InitChip8(state);
 
-    int size = LoadIntoMemory("/home/nun/Programming/C/Chip8/roms/tetris.ch8", state, 0);
+    int size = LoadIntoMemory("/home/nun/Programming/C/Chip8/roms/logo.ch8", state, 0);
     printf("Loaded %d bytes.", size);
 
     while(1) {
@@ -25,7 +25,7 @@ int main() {
 
         state->draw ^= 1;
         RunChip8(state);
-        usleep(250);
+        usleep(1000);
     }
 
     return 0;
